76de60c9bf80a26aa3d0477076a08228c927727c
sprindy <sprindy.liu@gmail.com>
2016-10-19 21:16:29 +0800

    [git][dotfiles] modify init_build_env.sh.
    Symptom: modify init_build_env.sh.
    Note: gtags.

--- init_build_env.sh
+++ init_build_env.sh
@@ -8,13 +8,14 @@ sudo apt-get install \
 	libgl1-mesa-dev libxml2-utils xsltproc unzip
 
 #Usefull
+# gtags:download from http://www.gnu.org/software/global/download.html and sudo make install
 sudo apt-get install \
 	vim android-tools-adb \
 	ssh make python-networkx \
 	zlib1g-dev:i386 lib32stdc++6 lib32bz2-1.0 \
-	git gitk meld \  #patch relate
-	ruby tree\  	   #(make[1]: ruby: Command not found) 
-	squashfs-tools \#mksquashfs
-	ctags global \  #gtags
+	git gitk meld \
+	ruby tree dos2unix \
+	squashfs-tools \
+	ctags texinfo \
 	samba smbclient silversearcher-ag \
 	python-usb
